659de3aaa49923d9d328e319699498fa5fa2b444
Arthur Costa <53489842+TheCodeCarrier@users.noreply.github.com>
2019-07-30 18:54:32 -0300

    arquivos adicionados

---
+++ CJ - Calculadora Java/src/calcw7/CalcW7.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcw7;
+
+/**
+ *
+ * @author Aluno Tarde
+ */
+public class CalcW7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calc calc = new Calc();
+        calc.setVisible(true);
+    }
+    
+}
